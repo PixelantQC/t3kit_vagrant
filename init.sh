3a22e32ab3209fe3b0f2ccab6a9c45090b8c7044
Anton Perebyinis <PixelantQC@users.noreply.github.com>
2016-04-21 11:19:09 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -15,7 +15,7 @@ if [ ! -d "site" ]; then
 fi
 cd site
 if [ ! -d .git ]; then
-    git clone https://github.com/PixelantQC/t3kit-1.git .
+    git clone https://github.com/t3kit/t3kit.git .
     git submodule update --init --recursive
 fi;
 cd TYPO3.CMS
